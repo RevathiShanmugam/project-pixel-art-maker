95e8a61026ba191ed76fac6325c20e77c8a64604
Revathi Shanmugam <srevathikarthik@gmail.com>
2018-08-13 12:43:48 -0700

    Code for makeGrid function

--- designs.js
+++ designs.js
@@ -12,8 +12,19 @@ const pixelCanvas = $('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+//makeGrid() function
+function makeGrid(w,h) {
+    pixelCanvas.children().remove();
+    const tr_html = '<tr></tr>';
+    const td_html = '<td style="background-color:#FFFFFF;"><span style="width:10px;height:10px;"></span></td>';
+    let tr = $(tr_html);
+    let td = $(td_html);
+    for (let rows = 0; rows < w; rows++){
+      tr.remove();
+      for (let cols = 0; cols < h; cols++) {
+        tr.append(td_html);
+      }
+      pixelCanvas.append(tr);
+      tr = $(tr_html);
+    }
+  }
